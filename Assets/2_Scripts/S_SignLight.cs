0a2ca64ec11a23b8692fa0c8b3d6e2de0128e6de
DYC20 <yd.carolla@gmail.com>
2025-10-13 16:07:05 +0300

    Store S_sign flicker light WIP basic script is done

---
+++ Assets/2_Scripts/S_SignLight.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_SignLight : MonoBehaviour
+{
+    [SerializeField] private List<Transform> sLightLocations; 
+    [SerializeField] private List<Transform> arrowLightLocations;
+    [SerializeField] private float flickerDuration;
+    [SerializeField] private float zOffset;
+    
+    private int currentLocationIndex = 0;
+    private float timer = 0.0f;
+
+    private Vector3 offset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        offset = new Vector3(0, 0, zOffset);
+        timer = flickerDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowS();
+    }
+
+    private void FollowS()
+    {
+        timer -= Time.deltaTime;
+        transform.position = sLightLocations[currentLocationIndex].position + offset;
+        if (timer<=0f)
+        {
+            currentLocationIndex = (currentLocationIndex + 1) % sLightLocations.Count;
+            timer = flickerDuration;
+        }
+        
+        
+    }
+}
